b41fea1481739b6f91c55080d0243f9d449abca5
iuzairaziz <72655773+iuzairaziz@users.noreply.github.com>
2021-01-02 04:55:43 -0800

    xyz

--- src/services/GenericService.js
+++ src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://web-lab-backend.herokuapp.com/";
+axios.defaults.baseURL = "https://web-lab-backend.herokuapp.com/api";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 
 class GenericService {
